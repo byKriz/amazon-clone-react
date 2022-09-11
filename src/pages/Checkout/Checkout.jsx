a4f3ea3733497473261fa856aadf889a99839fc7
byKriz <cristianesquivelh@hotmail.com>
2022-09-11 15:30:09 -0400

    iniciando la page de checkout

---
+++ src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Checkout = () => {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
