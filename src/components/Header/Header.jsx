87e2b97d262d14207907f6ba370b24676502c14f
byKriz <cristianesquivelh@hotmail.com>
2022-09-11 04:08:32 -0400

    maquetando el html

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import style from "./Header.module.scss";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <div>Logo</div>
+      <div>Location</div>
+      <div>Search Bar</div>
+      <div>
+        right nav
+        <div>
+          nav tools
+          <a href="">account</a>
+          <a href="">orders history</a>
+          <a href="">buy cart</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
