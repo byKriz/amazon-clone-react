7b17839ee3ad071e5a12618696e71275c2fa7534
byKriz <cristianesquivelh@hotmail.com>
2022-09-11 19:29:23 -0400

    aplicando estilos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 import { GoSearch } from "react-icons/go";
-import {IoMdArrowDropdown} from "react-icons/io"
+import { IoMdArrowDropdown } from "react-icons/io";
 
 export const Header = () => {
   return (
     <div className={styles.navBar}>
-      <div>
+      <div className={styles.logoContainer}>
         <Link to="/">
           <img
             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
@@ -20,30 +20,37 @@ export const Header = () => {
         <form action="" method="GET" className={styles.navForm}>
           <div className={styles.navLeft}>
             <span className={styles.navSearchLabel}>All</span>
-            <IoMdArrowDropdown className={styles.navSearchLabelIcon}/>
+            <IoMdArrowDropdown className={styles.arrowIcon} />
           </div>
 
           <div className={styles.navFill}>
-            <input
-              type="text"
-              name="field-keywords"
-              className={styles.navInput}
-            />
+            <div className={styles.navSearchFill}>
+              <input
+                type="text"
+                name="field-keywords"
+                className={styles.navSearchInput}
+              />
+            </div>
           </div>
-
-          <div className={styles.navRight}>
-            <GoSearch />
+          <div className={styles.navRightSearch}>
+            <GoSearch className={styles.navRightSearchIcon} size={20} />
           </div>
         </form>
       </div>
-      <div>
-        right nav
-        <div>
-          nav tools
-          <a href="">account</a>
-          <a href="">orders history</a>
-          <a href="">buy cart</a>
-        </div>
+
+      <div className={styles.navRight}>
+        <Link to="/account" className={styles.accountSection}>
+          <span className={styles.accountLine_1}>Hello, sign in</span>
+          <span className={styles.accountLine_2}>
+            Account & List
+            <span>
+              <IoMdArrowDropdown className={styles.arrowIcon} />
+            </span>
+          </span>
+        </Link>
+
+        <a href="">orders history</a>
+        <a href="">buy cart</a>
       </div>
     </div>
   );
