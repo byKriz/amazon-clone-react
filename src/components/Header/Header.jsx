1a926745fa92dd200bd8553108d960a77b48b4cf
byKriz <cristianesquivelh@hotmail.com>
2022-09-11 15:29:36 -0400

    maquetando el header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,41 @@
 import React from "react";
-import style from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+import { GoSearch } from "react-icons/go";
+import {IoMdArrowDropdown} from "react-icons/io"
 
 export const Header = () => {
   return (
-    <nav>
-      <div>Logo</div>
-      <div>Location</div>
-      <div>Search Bar</div>
+    <div className={styles.navBar}>
+      <div>
+        <Link to="/">
+          <img
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            alt="amazon-logo"
+            className={styles.logo}
+          />
+        </Link>
+      </div>
+      <div className={styles.navSearch}>
+        <form action="" method="GET" className={styles.navForm}>
+          <div className={styles.navLeft}>
+            <span className={styles.navSearchLabel}>All</span>
+            <IoMdArrowDropdown className={styles.navSearchLabelIcon}/>
+          </div>
+
+          <div className={styles.navFill}>
+            <input
+              type="text"
+              name="field-keywords"
+              className={styles.navInput}
+            />
+          </div>
+
+          <div className={styles.navRight}>
+            <GoSearch />
+          </div>
+        </form>
+      </div>
       <div>
         right nav
         <div>
@@ -16,6 +45,6 @@ export const Header = () => {
           <a href="">buy cart</a>
         </div>
       </div>
-    </nav>
+    </div>
   );
 };
