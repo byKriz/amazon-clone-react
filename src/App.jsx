ae2058a078ddfeee38d16a441833b6580e65ab89
byKriz <cristianesquivelh@hotmail.com>
2022-09-11 04:08:22 -0400

    agregando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { Checkout } from "./pages/Checkout/Checkout";
 
 function App() {
-
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
