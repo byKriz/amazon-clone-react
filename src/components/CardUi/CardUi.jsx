f463de92a117829f49efe670529a9300252c031e
byKriz <cristianesquivelh@hotmail.com>
2022-09-14 18:56:31 -0400

    ajustando los estilos de las cartas

--- src/components/CardUi/CardUi.jsx
+++ src/components/CardUi/CardUi.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import styles from "./CardUi.module.scss";
 
-export const CardUi = ({ title = "Product 1" }) => {
+export const CardUi = ({ title = "Product 1", image }) => {
   return (
     <div className={styles.cardUi}>
-      <div className={styles.titleContainer}>
-        <h2 className={styles.title}>{title}</h2>
-      </div>
+      <div className={styles.cardContainer}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>{title}</h2>
+        </div>
 
-      <div className={styles.cardBody}>
-        <a href="/">
-          <div>
-            <img src="" alt="" />
-          </div>
-        </a>
-      </div>
+        <div className={styles.cardBody}>
+          <a href="/">
+            <div>
+              <img src={image} alt="" />
+            </div>
+          </a>
+        </div>
 
-      <div className={styles.cardFooter}>
-        <a href="">
-          Shop Now
-        </a>
+        <div className={styles.cardFooter}>
+          <a href="">Shop Now</a>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { CardUi } from "../../components/CardUi/CardUi";
 import { Header } from "../../components/Header/Header";
+import { CardsUiContainer } from "../../containers/CardsUiContainer/CardsUiContainer";
 
 export const Home = () => {
   return (
     <div>
       <Header />
-      <CardUi />
+      <CardsUiContainer>
+        <CardUi />
+        <CardUi
+          title={"922: Steelers vs. Browns"}
+          image="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2022/Other/Sports/TNF/GWtakeover/Desktop_Image-Card_week3_9-22_379x304._SY304_CB610971969_.jpg"
+        />
+        <CardUi />
+        <CardUi />
+      </CardsUiContainer>
     </div>
   );
 };
